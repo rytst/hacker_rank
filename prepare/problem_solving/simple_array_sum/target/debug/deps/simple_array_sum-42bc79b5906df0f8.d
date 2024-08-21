fc69578139b3a05afd2531c105c0888ddb195f48
rytst <r.saito0111@gmail.com>
2024-08-22 03:41:40 +0900

    add simple

---
+++ prepare/problem_solving/simple_array_sum/target/debug/deps/simple_array_sum-42bc79b5906df0f8.d
@@ -0,0 +1,5 @@
+/home/rytst/projects/hacker_rank/prepare/problem_solving/simple_array_sum/target/debug/deps/simple_array_sum-42bc79b5906df0f8: src/main.rs
+
+/home/rytst/projects/hacker_rank/prepare/problem_solving/simple_array_sum/target/debug/deps/simple_array_sum-42bc79b5906df0f8.d: src/main.rs
+
+src/main.rs:
---
+++ prepare/problem_solving/simple_array_sum/target/debug/simple_array_sum.d
@@ -0,0 +1,1 @@
+/home/rytst/projects/hacker_rank/prepare/problem_solving/simple_array_sum/target/debug/simple_array_sum: /home/rytst/projects/hacker_rank/prepare/problem_solving/simple_array_sum/src/main.rs
